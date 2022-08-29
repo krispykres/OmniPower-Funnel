b36d2f17f1c9c13f89906be1384e60d68ed700d9
Ian Kresyman <ikresyman@gmail.com>
2022-08-28 22:06:00 -0600

    adding fb pixel code

--- pages/_document.js
+++ pages/_document.js
@@ -14,6 +14,23 @@ export default function Document() {
                     })(window,document,'script','dataLayer','GTM-PHBB3B4');
                 `}
             </Script>
+            <Script id="facebook-pixel" strategy="afterInteractive">
+                {`
+                    !function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', '704719686821979');
+                    fbq('track', 'PageView');
+                `}
+            </Script>
+            <noscript><img height="1" width="1" style="display:none"
+                src="https://www.facebook.com/tr?id=704719686821979&ev=PageView&noscript=1"
+            /></noscript>
 			<title>Omni Power Solar Quote</title>
 			<meta name="description" content="Get your FREE quote with Omni Power" />
 			<link rel="icon" href="/favicon.ico" />
